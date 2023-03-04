5b69928e6f56ad3dbd522129dbdd967add19211e
monu kumar <69889172+mkagarwal235@users.noreply.github.com>
2023-03-05 00:20:12 +0530

    Add crypto js for storing password

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,13 +1,15 @@
 import user from "@/models/users";
 import connectDB from "../middleware/mongooes";
-
+var CryptoJS = require("crypto-js");
 const handler = async (req, res) => {
     if (req.method == 'POST') {
         console.log(req.body)
         let users = await user.findOne({ "email": req.body.email })
+        const bytes=CryptoJS.AES.decrypt(users.password,"secret123");
+        let decryptedPass=bytes.toString(CryptoJS.enc.Utf8);
         if (users) {
 
-            if (req.body.email == users.email && req.body.password == users.password) {
+            if (req.body.email == users.email && req.body.password == decryptedPass) {
                 res.status(200).json({ success: true, email: users.email, name: users.name })
 
             }
--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,10 +1,12 @@
 import user from "@/models/users";
 import connectDB from "../middleware/mongooes";
+var CryptoJS = require("crypto-js");
 
 const handler= async (req,res)=>{
     if(req.method=='POST')
     {  console.log(req.body)
-        let u=new user(req.body);
+        const {name,email}=req.body
+        let u=new user({name,email,password: CryptoJS.AES.encrypt(req.body.password, "secret123").toString()});
        await u.save(); 
        
         res.status(200).json({success:"success"})
